5727ee86dba7c5b3995c6632246c8bd5e116588a
huang yang <elious.huang@gmail.com>
2017-09-14 18:10:12 +0800

    complete tassert unit test

--- tests/test_assert.cpp
+++ tests/test_assert.cpp
@@ -1,8 +1,24 @@
 #include "gtest/gtest.h"
+#include "../src/tassert.h"
+
+int AssertFunc(int val)
+{
+    T_ASSERT(val == 3);
+
+    return 2;
+}
+
+TEST(AssertTest, Assert)
+{
+    ASSERT_DEATH(AssertFunc(2), "");
+    ASSERT_EQ(2, AssertFunc(3));
+}
+
 
 int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
+    testing::FLAGS_gtest_death_test_style = "fast";
     return RUN_ALL_TESTS();
 }
 
