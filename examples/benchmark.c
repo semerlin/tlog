ae07b14598f4f7c11dde03e151b8004cf5a57bea
huang yang <elious.huang@gmail.com>
2017-10-14 15:58:13 +0800

    add benchmark example

--- examples/benchmark.c
+++ examples/benchmark.c
@@ -1,9 +1,165 @@
+/**
+ * Copyright 2017, Huang Yang <elious.huang@gmail.com>. All rights reserved.
+ *
+ * See the COPYING file for the terms of usage and distribution.
+ */
 #include "tlog/tlog.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <sys/time.h>
+#include <signal.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <limits.h>
+
+
+#define DEFAULT_TIME  5
+
+static unsigned char is_testing = false;
+
+/* configure buffer */
+static const char benchmark_cfg[] = "[general]\n[format]\nstdfmt=\"%d(%Y-%m-%d %T) %-6V [%F:%L] %m\"\n[rules]\nstd.*=stdfmt;>stdout\nfile.*=/tmp/tlog_benchmark.log\npipe.*=| cat > /dev/null\n";
+
+/* timeout signal process */
+static void alarm_act(int sig)
+{
+    is_testing = false;
+}
+
+
+/**
+ * @brief init benchmark
+ */
+static void benchmark_init(void)
+{
+    struct sigaction act;
+    act.sa_handler = &alarm_act;
+    sigaction(SIGALRM, &act, 0);
+}
+
+static int calc_speed(const struct timeval *start, 
+        const struct timeval *stop, double count)
+{
+    if (0 == count)
+    {
+        return 0;
+    }
+
+    double secs = difftime(stop->tv_sec, start->tv_sec);
+    return count * 1000000 / (secs * 1000000 + (double)(stop->tv_usec - start->tv_usec));
+}
+
+static void internal_test(unsigned int time, const tlog_category *cat, 
+        struct timeval *start, struct timeval *stop, double *count)
+{
+    *count = 0;
+
+    is_testing = true;
+    alarm(time);
+    /* record start time */
+    gettimeofday(start, NULL);
+    while(is_testing)
+    {
+        tlog_debug(cat, "this is tlog speed test\r");
+        *count += 1;
+    }
+    /* record stop time */
+    gettimeofday(stop, NULL);
+}
+
+
+static void printf_benchmark_result(const struct timeval *start, 
+        const struct timeval *stop, double count)
+{
+    struct tm begin_tm, end_tm;
+    char time_buf[32];
+    /* print bench mark result */
+    fprintf(stdout, "-------------------------------------------------------------------------------------\n");
+    fprintf(stdout, "|         start time         |         stop time          |      average speed      |\n");
+    fprintf(stdout, "|----------------------------|----------------------------|-------------------------|\n");
+    fprintf(stdout, "| ");
+    localtime_r(&start->tv_sec, &begin_tm);
+    strftime(time_buf, 32, "%Y-%m-%d %T", &begin_tm);
+    fprintf(stdout, "%s.%06d", time_buf, (int)(start->tv_usec));
+    fprintf(stdout, " | ");
+    localtime_r(&stop->tv_sec, &end_tm);
+    strftime(time_buf, 32, "%Y-%m-%d %T", &end_tm);
+    fprintf(stdout, "%s.%06d", time_buf, (int)(stop->tv_usec));
+    fprintf(stdout, " | ");
+    fprintf(stdout, "   %07d logs/secs    |\n", calc_speed(start, stop, count));
+    fprintf(stdout, "-------------------------------------------------------------------------------------\n");
+}
+
+static void test_stdout(unsigned int time)
+{
+    fprintf(stdout, "\e[32mtesting stdout speed....\e[0m\n");
+    const tlog_category *cat_std = tlog_get_category("std");
+    struct timeval tv_begin, tv_end;
+    double count = 0;
+
+    /* run test */
+    internal_test(time, cat_std, &tv_begin, &tv_end, &count);
+    fprintf(stdout, "\n");
+
+    /* print bench mark result */
+    printf_benchmark_result(&tv_begin, &tv_end, count);
+}
+
+
+static void test_file(unsigned int time)
+{
+    fprintf(stdout, "\e[32mtesting file speed....\e[0m\n");
+    const tlog_category *cat_file = tlog_get_category("file");
+    struct timeval tv_begin, tv_end;
+    double count = 0;
+
+    /* run test */
+    internal_test(time, cat_file, &tv_begin, &tv_end, &count);
+
+    /* print bench mark result */
+    printf_benchmark_result(&tv_begin, &tv_end, count);
+}
+
+
+
+static void test_pipeline(unsigned int time)
+{
+    fprintf(stdout, "\e[32mtesting pipeline speed....\e[0m\n");
+    const tlog_category *cat_pipe = tlog_get_category("pipe");
+    struct timeval tv_begin, tv_end;
+    double count = 0;
+
+    /* run test */
+    internal_test(time, cat_pipe, &tv_begin, &tv_end, &count);
+
+    /* print bench mark result */
+    printf_benchmark_result(&tv_begin, &tv_end, count);
+}
 
 int main(int argc, char **argv)
 {
-    tlog_init((void *)0);
-    const tlog_category *cat = tlog_get_category("*");
-    tlog_debug(cat, "hello tlog");
+    char *endptr = NULL, *str = NULL;
+    unsigned int time = DEFAULT_TIME;
+    long val = 0;
+    if (argc > 1)
+    {
+        str = argv[1];
+        val = strtol(str, &endptr, 10);
+        if ((0 == errno) && (endptr != str))
+        {
+            time = val;
+        }
+    }
+    benchmark_init();
+    tlog_init_from_data(benchmark_cfg);
+
+    test_pipeline(time);
+    test_stdout(time);
+    test_file(time);
+
     return 0;
 }
